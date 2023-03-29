06ab43cbc360a7cbb1634c0ed6806f57e709263f
Mariia Gumenyuk <sana22_gmd@student.ztu.edu.ua>
2023-03-29 12:29:44 +0300

    done

---
+++ Prakt1/Task1/Controllers/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task1.Models;
+
+namespace Task1.Controllers
+{
+	public class HelloController : Controller
+	{
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult Result(User user)
+		{
+			return View(user);
+		}
+
+	}
+}
---
+++ Prakt1/Task1/Models/User.cs
@@ -0,0 +1,7 @@
+﻿namespace Task1.Models
+{
+    public class User
+    {
+        public string Name { get; set; }  
+    }
+}
